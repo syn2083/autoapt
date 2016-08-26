c82377d8c737946c0a13cbc893bb15fe9e4a5613
syn2083 <syndicate2083@gmail.com>
2016-08-26 08:15:04 -0400

    Testing

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 __author__ = 'Syn'
 import os
-
+import sys
 
 BASE_DIR = os.getcwd()
 
@@ -12,3 +12,12 @@ log_file = os.path.join(log_dir, 'aptdemo.log')
 SERV_ADDR = '127.0.0.1'
 SOCK_PORT = 8091
 CHUNK_SIZE = 4096
+
+# System Configs
+DEMO_CONF_DIR = os.path.join(os.getcwd(), '\\config_files')
+DEMO_CONF_FILE = os.path.join(DEMO_CONF_DIR, 'demo.config')
+JIF_CONF_FILE = os.path.join(DEMO_CONF_DIR, 'jif.config')
+
+def init_configs():
+    if not os.path.exists(DEMO_CONF_DIR):
+        os.makedirs(DEMO_CONF_DIR)
